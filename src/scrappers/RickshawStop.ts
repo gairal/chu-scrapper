05d8279acad44f84208aa8b422b2f166e4b3d9e0
frank.gairal <frank@autofi.io>
2020-02-05 01:17:40 -0800

    fix(rick): simpler selector

--- src/scrappers/RickshawStop.ts
+++ src/scrappers/RickshawStop.ts
@@ -16,26 +16,23 @@ export default class RickshawStop extends Scrapper<Event> {
     try {
       const $ = load(data);
       const events: Event[] = [];
-      $('article.calendar-view table').each((_, t) => {
-        $(t).find('td.data.has-event .one-event').each((__, d) => {
-          const $d = $(d);
-          if ($d.find('.sold-out').length) return;
+      $('.one-event').each((_, d) => {
+        const $d = $(d);
+        if ($d.find('.sold-out').length) return;
 
-          const $headline = $d.find('.event-name a');
-          const name = $headline.text();
-          events.push({
-            date: new Date($d
-              .parent()
-              .find('.date .value-title')
-              .attr('title') as string),
-            description: $d
-              .find('.supports a')
-              .map((___, s) => $(s).text()).get().join(),
-            img: $d.find('.image-url img').attr('src'),
-            name,
-            url: `${BASE_URL}${$headline.attr('href')}`,
-            venue: Venue.RickshawStop,
-          });
+        const $headline = $d.find('.event-name a');
+        events.push({
+          date: new Date($d
+            .parent()
+            .find('.date .value-title')
+            .attr('title') as string),
+          description: $d
+            .find('.supports a')
+            .map((__, s) => $(s).text()).get().join(),
+          img: $d.find('.image-url img').attr('src'),
+          name: $headline.text(),
+          url: `${BASE_URL}${$headline.attr('href')}`,
+          venue: Venue.RickshawStop,
         });
       });
 
