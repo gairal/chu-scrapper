f1111660398996ec462b16fdd7bcabe31b829326
frank.gairal <frank@autofi.io>
2020-07-19 02:13:16 -0700

    fix: add jest file

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  bail: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts'],
+  coveragePathIgnorePatterns: ['.d.ts'],
+  errorOnDeprecated: true,
+  logHeapUsage: true,
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  moduleNameMapper: {
+    'Mocks(.*)$': '<rootDir>/test/__mocks__$1',
+    '~/(.*)$': '<rootDir>/src/$1',
+  },
+  preset: 'ts-jest',
+  verbose: false,
+};
